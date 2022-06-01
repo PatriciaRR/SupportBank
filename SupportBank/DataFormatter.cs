44ff682696130655674c5f768c13f00e29c3c195
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-06-01 14:46:31 +0100

    log out each line of transactions2014

--- SupportBank/DataFormatter.cs
+++ SupportBank/DataFormatter.cs
@@ -1,15 +1,23 @@
 
-        // get transactions file 
-        // split file into separate lines
-        // format each line into a transaction
-        // return a list of transactions 
+// get transactions file 
+// split file into separate lines
+// format each line into a transaction
+// return a list of transactions 
 namespace SupportBank
 {
     class DataFormatter
     {
         public List<Transaction> TransactionReader()
         {
-            return new List<Transaction>{};
+            string path = @".\Transactions2014.csv";
+
+            string[] lines = System.IO.File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            return new List<Transaction> { };
         }
     }
 
